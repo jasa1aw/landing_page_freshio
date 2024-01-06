2fb6a3092459819a26e5333105b21354ae8a203e
jasa1aw <erakrut05@gmail.com>
2024-01-06 23:36:10 +0600

    edited somethings

--- js/slider.js
+++ js/slider.js
@@ -1,5 +1,3 @@
-const prevArrow = document.getElementById('prevArrow');
-const nextArrow = document.getElementById('nextArrow');
 $('.feature-items').slick({
     infinite: true,
     slidesToShow: 4,
@@ -8,8 +6,6 @@ $('.feature-items').slick({
     nextArrow: nextArrow,
     customPaging: true
 });
-const prevArrow2 = document.getElementById('prevArrow2');
-const nextArrow2 = document.getElementById('nextArrow2');
 $('.feature-items-2').slick({
     infinite: true,
     slidesToShow: 4,
